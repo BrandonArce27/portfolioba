4d9baf696e7610e5a730ebfb8eff15ff9148501a
BrandonArce27 <brandonarce2712@gmail.com>
2024-05-15 22:37:14 +0000

    vercel analytics changes app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { useEffect } from "react";
 import Home from "./components/Home";
 import NavBar from "./components/Nav";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AboutMe from "./components/AboutMe";
 import NotFound from "./components/NotFound";
 import Skills from "./components/Skills";
 import Project from "./components/Project";
 import { Analytics } from '@vercel/analytics/react'; // Import Analytics component
 
+// import aos : for animation
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+
 function App() {
+  useEffect(() => {
+    Aos.init({
+      duration:1800,
+      offset:100
+    });
+  }, []);
+
   return (
     <Router>
       <div className="App">
